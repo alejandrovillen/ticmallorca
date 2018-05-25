b66a6c1717f7b97eb18c38fba2be1eb58b45e230
Alejandro Villén <alejandro.vildez@gmail.com>
2018-05-25 11:39:51 +0200

    Add press function and bin/bash

--- regweb.sh
+++ regweb.sh
@@ -3,9 +3,11 @@
 # RegWeb3 installation
 
 #Dependences
+	read -p "Press to install dependences"
 	apt-get install -y git unzip make python postgresql ant ant-optional
 
 # Environment variables
+	read -p "Press to set environment variables"
 	echo 'JBOSS="/usr/local/jboss-as"' | sudo tee --append /etc/environment
 	echo 'PACKAGES="/opt/regweb/packages"' | sudo tee --append /etc/environment
 	echo 'TICPACK="/opt/regweb/packages/ticmallorca"' | sudo tee --append /etc/environment
@@ -13,6 +15,7 @@
 
 
 # Directory
+	read -p "Press to create directories"
 	mkdir -p /opt/regweb
 	mkdir -p $PACKAGES
 	mkdir -p $PACKAGES/RegWeb
@@ -27,11 +30,12 @@
 	mkdir -p /usr/lib/jvm
 
 # Init
+	read -p "Press to init TICPACK"
 	mv ./dist/* $TICPACK
 	
 # Packages
-	cd $PACKAGES
 	## JDK
+		read -p "Press to install JDK"
 		cp -R $TICPACK/JDK/jdk1.7.0_80 /usr/lib/jvm/
 		# Update java versions (https://gist.github.com/senthil245/6093389)
 		sudo update-alternatives --install "/usr/bin/java" "java" \
@@ -43,6 +47,7 @@
 		# Select java 1.7 version
 		sudo update-alternatives --config java
 	## RegWeb3 release 3.0.9
+		read -p "Press to install RegWeb3"
 		cd $PACKAGES/RegWeb
 		wget https://github.com/GovernIB/registre/releases/download/registre-3.0.9/release-regweb3-3.0.9.zip
 		wget https://github.com/GovernIB/registre/archive/registre-3.0.9.zip
@@ -50,26 +55,32 @@
 		unzip release-regweb3-3.0.9.zip
 		cp -r ./release-regweb3-3.0.9/* ./regweb
 	## JBoss 5.1.0 GA
+		read -p "Press to install JBoss 5.1.0 GA"
 		cp -r $TICPACK/JBoss/* $JBOSS
 	## JBoss Bug Fix
+		read -p "Press to install JBoss Fix"
 		cd $PACKAGES/JBossBug
 		wget https://repository.jboss.org/nexus/content/repositories/root_repository/jboss/metadata/1.0.6.GA-brew/lib/jboss-metadata.jar
 		# Apply the JBoss Bug Fix
 		cp -r $PACKAGES/JBossBug/* $JBOSS/common/lib/
 		cp  -r $PACKAGES/JBossBug/* $JBOSS/client/
 	## JBoss CXF
+		read -p "Press to install JBoss CXF"
 		cp -r $TICPACK/JBossCXF/* $JBOSS
 	## Color-JUnit
+		read -p "Press to install Color-JUnit"
 		cd $PACKAGES/ColorJUnit
 		git clone https://github.com/eevans/color-junit
 		cd color-junit
 		make install
 	## Postgresql Driver
+		read -p "Press to install PostgresDriver"
 		cd $PACKAGES/Postgresql_driver
 		wget http://jdbc.postgresql.org/download/postgresql-8.4-703.jdbc3.jar
 		cp -r $PACKAGES/Postgresql_driver/postgresql-8.4-703.jdbc3.jar -P $JBOSS/server/default/lib/
 
 # Settings
+	read -p "Press to configure settings"
 	## CXF content and config
 		cp $TICPACK/App/regweb/ant.properties $JBOSS
 	## Update build-testsuite.xml
@@ -110,6 +121,7 @@
 
 # Services
 	## JBOSS Service
+		read -p "Press to setup JBoss service"
 		cp $TICPACK/App/regweb/jboss /etc/init.d/
 		chmod 777 /etc/init.d/jboss
 		update-rc.d jboss defaults
